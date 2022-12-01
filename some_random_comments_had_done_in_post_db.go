88afdb2db573e25c103715ae6eee12ed1566547a
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2022-12-01 17:09:33 +0530

    Update some_random_comments_had_done_in_post_db.go

--- some_random_comments_had_done_in_post_db.go
+++ some_random_comments_had_done_in_post_db.go
@@ -17,6 +17,6 @@ add_somethine_wanna_say("It was very shocking for me, i was in that state then i
 add_somethine_wanna_say("primary option is my job and its shame to say some peoples are spoiling my things, my opporunities, jobs")
 add_somethine_wanna_say("I am giving you one example when i was in last sem those stupid peoples my project members who simply don't know how to talk with professors, how to write simple code in C or any")
 add_somethine_wanna_say("With low grads they are working as a Software Engineer or Senior Software Engineer in Infosys or Persistent, I am really derserving person with good performance and if they are spoiling others opportunities they are enjoying that moments currently but")
-add_somethine_wanna_say("just think and i am daily thinkging that after my parents there will no one for me, my family members won't support me and also my own brother won'nt help me for some reasons either he will go to UK or Ireland after so where can i go after just think")
+add_somethine_wanna_say("just think and i am daily thinking that after my parents there will no one for me, my family members won't support me and also my own brother won'nt help me for some reasons either he will go to UK or Ireland after so where can i go after just think")
 
 "
