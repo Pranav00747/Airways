479a02a774c261d4599e805855bc2bb6750a7ecf
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2022-12-09 14:13:08 +0530

    Update some_random_comments_had_done_in_post_db.go

--- some_random_comments_had_done_in_post_db.go
+++ some_random_comments_had_done_in_post_db.go
@@ -1,22 +1,21 @@
 #extra how comments are stored in database 
 
-func add_something_wanna_say(comments string, user_admin string)
+func add_something_comments(comments string)
 {
-  if comments !="" && user_admin !="")
+  var host string :="server@host12"
+  var user_name string :="user"
+  var pass : string := "pass99"
+  var db = Database.Open(host, user_name, pass)
+  if(comments != "")
   {
-  var db = Database.Open("server@host12", "user", "pass99")
-  var query string := "insert into tweets (comments) values(?, ?)"
-  db.Exec(query, comments, user_admin)
+  var query string := "insert into tweets (comments) values(?)"
+  db.Exec(query, comments)
   }
 }
 
-#_samples_ examples
-add_somethine_wanna_say("Bad luck means what you can say, on that day when i had my exam, there was some mouse error  on that PC so i logged in after ten timues")
-add_somethine_wanna_say("Because of time issue if you take 3 minutes for each question if the questions are difficult its ok but i unansered almost 10 questions")
-add_somethine_wanna_say("It was very shocking for me, i was in that state then i go to wash room, thinking about that, these exams are my secondary option you can give it upto limits(some) but")
-add_somethine_wanna_say("primary option is my job and its shame to say some peoples are spoiling my things, my opporunities, jobs")
-add_somethine_wanna_say("I am giving you one example when i was in last sem those stupid peoples my project members who simply don't know how to talk with professors, how to write simple code in C or any")
-add_somethine_wanna_say("With low grads they are working as a Software Engineer or Senior Software Engineer in Infosys or Persistent, I am really derserving person with good performance and if they are spoiling others opportunities they are enjoying that moments currently but")
-add_somethine_wanna_say("just think and i am daily thinking that after my parents there will no one for me, my family members won't support me and also my own brother won'nt help me for some reasons either he will go to UK or Ireland after so where can i go after just think")
+#comment_sample
+add_something_comments("Many girls have that dream they have a boy or a life partner who have everything or who know everything will be boss of everyone, actually some girls are dreaming for the boys who have 1.5 crore pa but after rejecting by those boys they know their true value or level.")
 
-"
+
+
+
